2e71e3855b233959ec5645a7072606fde1c5e17d
nightSanches <galesavich@gmail.com>
2024-12-08 16:24:49 +0500

    Созданы классы ViewModelMessage, ViewModelSend

---
+++ Common/ViewModelMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ViewModelMessage
+    {
+        public string Command { get; set; }
+        public string Date { get; set; }
+        public ViewModelMessage(string Command, string Data) {
+            this.Command = Command;
+            this.Date = Data;
+        }
+    }
+}
