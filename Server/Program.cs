c264e1de2aaccd64f2091e4ea67000bc1010b8d9
nightSanches <galesavich@gmail.com>
2024-12-08 16:32:28 +0500

    Метод AutorizationUser. Савичев

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
 {
-    internal class Program
+    public class Program
     {
+        public static List<User> Users = new List<User>();
+        public static IPAddress IpAddress;
+        public static int Port;
         static void Main(string[] args)
         {
+
+        }
+
+        public static bool AutorizationUser(string login, string password)
+        {
+            User user = null;
+            user = Users.Find(x => x.login == login && x.password == password);
+            return user != null;
         }
     }
 }
