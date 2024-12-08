ba83a33f7046f8f326360cb6e3471fc28731e396
nightSanches <galesavich@gmail.com>
2024-12-08 16:25:56 +0500

    Создан класс FileInfoFTP. Савичев

---
+++ Common/FileInfoFTP.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class FileInfoFTP
+    {
+        public byte[] Data { get; set; }
+        public string Name { get; set; }
+        public FileInfoFTP(byte[] Data, string Name)
+        {
+            this.Data = Data;
+            this.Name = Name;
+        }
+    }
+}
