cfb175c5bbe1c281efc227f4c223de93e86c6f1c
Hat <cool97438@gmail.com>
2024-12-08 18:24:51 +0500

    разработаннный метод ConnectServer. Kurlishuk

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
+using Common;
+using Newtonsoft.Json;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
 
 namespace Client
 {
@@ -81,7 +87,7 @@ namespace Client
                         // Команда верная
                         BCommand = true;
 
-                }            
+                }
             }
             return BCommand;
         }
@@ -90,8 +96,135 @@ namespace Client
 
 
 
+        public static void ConnectServer()
+        {
+            try
+            {
+                // Создаём конечную точку, состоящую из IP-адреса и порта
+                IPEndPoint endPoint = new IPEndPoint(IPAddress, Port);
+                // Создаём сокет для подключения
+                Socket socket = new Socket(
+                // Задаём схему адресации, которую будет использовать сокет ІРѵ4
+                AddressFamily.InterNetwork,
+                // Задаём тип сокета, двоичный код
+                SocketType.Stream,
+                // Указываем протокол сокета
+                ProtocolType.Tcp);
+                // Подключаемся к серверу
+                socket.Connect(endPoint);
+                // Если состояние подключено
+                if (socket.Connected)
+                {
+                    // Изменяем цвет текста в консоле
+                    Console.ForegroundColor = ConsoleColor.White;
+                    // Ждём сообщения от пользователя
+                    string message = Console.ReadLine();
+                    // Проверяем сообщение пользователя на соответствие команд
+                    if (CheckCommand(message))
+                    {
+                        // Создаём модель, в которой хранится сообщние пользователя, и ID пользователя
+                        ViewModelSend viewModelSend = new ViewModelSend(message, Id);
+                        // Если команда set
+                        if (message.Split(new string[1] { " " }, StringSplitOptions.None)[0] == "set")
+                        {
+                            // Разбиваем сообщение на данные
+                            string[] DataMessage = message.Split(new string[1] { " " }, StringSplitOptions.None);
+                            // Собираем наименование файла, аналагично сервера
+                            string NameFile = "";
+                            for (int i = 1; i < DataMessage.Length; i++)
+                                if (NameFile == "")
+                                    NameFile += DataMessage[i];
+                                else
+                                    NameFile += " " + DataMessage[i];
+                            // проверяем существование файла
+                            if (File.Exists(NameFile))
+                            {
+                                // Получаем информаццию о файле
+                                FileInfo FileInfo = new FileInfo(NameFile);
+                                // Создаём объект состоящий из наименования файл и массива байт
+                                FileInfoFTP NewFileInfo = new FileInfoFTP(File.ReadAllBytes(NameFile), FileInfo.Name);
+                                // Создаём модель, состающую из данных о файле и коде пользователя
+                                viewModelSend = new ViewModelSend(JsonConvert.SerializeObject(NewFileInfo), Id);
+                            }
+                            else
+                            {
+                                // Изменяем цвет текста в консоле
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                // Выводим текст
+                                Console.WriteLine("Указанный файл не существует");
+                            }
+                        }
+                        // Разбиваем сообщения на массив байт, предварительно преобразов в Json
+                        byte[] messageByte = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelSend));
+                        // Отправляем сообщение
+                        int BytesSend = socket.Send(messageByte);
+                        // Создаём массив байт
+                        byte[] bytes = new byte[10485760];
+                        // Получаем ответ от сервера
+                        int BytesRec = socket.Receive(bytes);
+                        // Преобразовываем ответ в строку
+                        string messageServer = Encoding.UTF8.GetString(bytes, 0, BytesRec);
+                        // Преобразовываем Json в объект
+                        ViewModelMessage viewModelMessage = JsonConvert.DeserializeObject<ViewModelMessage>(messageServer);
+                        // Если метод авторизации
+                        if (viewModelMessage.Command == "autorization")
+                            // Запоминаем код пользоватля
+                            Id = int.Parse(viewModelMessage.Date);
+                        // Если сообщение
+                        else if (viewModelMessage.Command == "message")
+                            // Выводи на экран
+                            Console.WriteLine(viewModelMessage.Date);
 
+                        // Если команда перехода в категорию
+                        else if (viewModelMessage.Command == "cd")
+                        {
+                            // Создаём список
+                            List<string> FoldersFiles = new List<string>();
+                            // Преобразовываем список пришедший с сервера в список
+                            FoldersFiles = JsonConvert.DeserializeObject<List<string>>(viewModelMessage.Date);
+                            // Выводим папки и файлы
+                            foreach (string Name in FoldersFiles)
+                                Console.WriteLine(Name);
+                        }
+
+                        // Если команда файл
+                        else if (viewModelMessage.Command == "file")
+                        {
+                            // Получаем имя файла из команды пользователя
+                            string[] DataMessage = viewModelSend.Message.Split(new string[1] { " " }, StringSplitOptions.None);
+                            // Собираем наименование файла, аналагично сервера
+                            string getFile = "";
+                            for (int i = 1; i < DataMessage.Length; i++)
+                                if (getFile == "")
+                                    getFile = DataMessage[i];
+                                else
+                                    getFile += " " + DataMessage[i];
+                            // Преобразовываем набор байт в массив из Json
+                            byte[] byteFile = JsonConvert.DeserializeObject<byte[]>(viewModelMessage.Date);
+                            // Сохраняем файл на клиенте
+                            File.WriteAllBytes(getFile, byteFile);
+                        }
+                    }
+                }
+                else
+                {
+                    // Изменяем цвет текста в консоле
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    // Выводим текст
+                    Console.WriteLine("Подключение не удалось.");
+                }
+                socket.Close();
+            }
+            catch (Exception exp)
+            {
+                // Изменяем цвет текста в консоле
+                Console.ForegroundColor = ConsoleColor.Red;
+                // Выводим текст
+                Console.WriteLine($"Что-то случилось: + {exp.Message}");
+            }
+        }
     }
 }
-                    
+
+
 
