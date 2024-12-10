a06129014cb19db0966cca28a014cf4e26dac261
nightSanches <galesavich@gmail.com>
2024-12-11 02:24:06 +0500

    Код MainWindows. Савичев

--- ClientWPF/MainWindow.xaml.cs
+++ ClientWPF/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,9 +12,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace ClientWPF
 {
@@ -20,9 +25,63 @@ namespace ClientWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
+        public IPAddress ipAddress;
+        public int port;
+        public int userId = -1;
+        public Stack<string> directoryStack = new Stack<string>();
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            OpenPage(new LoginPage());
+        }
+
+        public void OpenPage(Page page)
+        {
+            DoubleAnimation startAnimation = new DoubleAnimation();
+            startAnimation.From = 1;
+            startAnimation.To = 0;
+            startAnimation.Duration = TimeSpan.FromSeconds(0.1);
+            startAnimation.Completed += delegate
+            {
+                frame.Navigate(page);
+                DoubleAnimation endAnimation = new DoubleAnimation();
+                endAnimation.From = 0;
+                endAnimation.To = 1;
+                endAnimation.Duration = TimeSpan.FromSeconds(0.1);
+                frame.BeginAnimation(OpacityProperty, endAnimation);
+            };
+            frame.BeginAnimation(OpacityProperty, startAnimation);
+        }
+
+        public ViewModelMessage SendCommand(string message)
+        {
+            try
+            {
+                IPEndPoint endPoint = new IPEndPoint(ipAddress, port);
+                using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    socket.Connect(endPoint);
+                    if (socket.Connected)
+                    {
+                        var request = new ViewModelSend(message, userId);
+                        byte[] requestBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(request));
+                        socket.Send(requestBytes);
+
+                        byte[] responseBytes = new byte[10485760];
+                        int receivedBytes = socket.Receive(responseBytes);
+                        string responseData = Encoding.UTF8.GetString(responseBytes, 0, receivedBytes);
+
+                        return JsonConvert.DeserializeObject<ViewModelMessage>(responseData);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка соединения: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            return null;
         }
     }
 }
